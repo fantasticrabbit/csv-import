d9fafe07da093417443d4e029396276a15431d4b
Tom Riley <thomriley@gmail.com>
2022-05-20 17:52:43 -0500

    get line function

--- openfile.mjs
+++ openfile.mjs
@@ -1,12 +1,7 @@
-import { promises as fs } from "fs";
-import { promises as readline } from "readline";
+import * as fs from "node:fs/promises";
+import * as readline from "node:readline/promises";
+import once from "events";
 import { stdin as input, stdout as output } from 'process';
-import pkg from 'papaparse';
-const { pkg: papa } = pkg;
-
-
-
-const rl = readline.createInterface({ input, output });
 
 const read_file=async(fname)=>{
     try{
@@ -14,7 +9,7 @@ const read_file=async(fname)=>{
         let stat=await file.stat();
         let buffer=Buffer.alloc(stat.size);
         let result=await file.read(buffer, 0, stat.size, null);
-        return result;
+        return result.buffer.toString();
 //        console.log(`Read ${result.bytesRead} bytes\nContents:\n${result.buffer.toString()}`);
     } catch(err) {
         console.log("ERROR", err);
@@ -22,17 +17,42 @@ const read_file=async(fname)=>{
 }
 
 const getfile=async()=>{
-    try {
-        let filenm=await rl.question("What file would you like to read? ");
-        rl.close();
-        return filenm;
-    } catch(err) {
-        console.log(err);
-        rl.close();
+    if (process.argv[2]===undefined) {
+        try {
+            const rl = readline.createInterface({ input, output });
+            let filenm=await rl.question("What file would you like to read? ");
+            rl.close();
+            return filenm;
+        } catch(err) {
+            console.log(err);
+            rl.close();
+        }
+    } else {
+        return process.argv[2];
     }
 }
 
+const getFirstLine=async (xfilex) => {
+    try {
+        const rl = await readline.createInterface({
+            input: fs.createReadStream(xfilex),
+            crlfDelay: Infinity
+          });
+    
+        rl.on('line', (line) => {
+            console.log(`Line from file: ${line}`);
+        });
+    
+        await once(rl, 'close');
+    
+        console.log('Header processed.');
+      } catch (err) {
+        console.error(err);
+      }
+}
+
 let filename=await getfile();
-let filedata=read_file(filename);
-let jsondata=papa.parse(filedata, {header: true});
-console.log(jsondata)
+let filedata=await read_file(filename);
+console.log(filedata);
+let header=await getFirstLine(filename);
+console.log(header);
